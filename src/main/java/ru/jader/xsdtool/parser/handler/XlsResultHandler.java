efcd174208b477264df62fabaceb0458c789ee54
eljader <pinkyjad@gmail.com>
2015-07-08 14:38:13 +0300

    refactor xls handler

--- src/main/java/ru/jader/xsdtool/parser/handler/XlsResultHandler.java
+++ src/main/java/ru/jader/xsdtool/parser/handler/XlsResultHandler.java
@@ -2,7 +2,9 @@ package ru.jader.xsdtool.parser.handler;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -21,18 +23,18 @@ public class XlsResultHandler implements Handler {
 	private static short STYLE_HEADER = 0;
 	private static short STYLE_TYPE = 2;
 	private static short STYLE_LONG_TEXT = 3;
-	
+		
 	private FileOutputStream writer; 
 	private Workbook workbook;
 	private Sheet sheet;
-	private Map<Short, CellStyle> style;
+	private Map<Short, CellStyle> styles;
 	private int rowNum;
 		
 	public XlsResultHandler(String filename) throws FileNotFoundException {
 		writer = new FileOutputStream(filename);
 		workbook = new XSSFWorkbook();
 		sheet = workbook.createSheet();
-		style = getStyles();
+		styles = createStyles();
 		rowNum = 0;
 		createHeader();
 	}
@@ -40,11 +42,10 @@ public class XlsResultHandler implements Handler {
 	public void handle(XsdUnit unit) {
 		Row row = sheet.createRow(getRowNum());
 		
-		addValue(row, 0, "", STYLE_LONG_TEXT);
-		addValue(row, 1, "", STYLE_TYPE);
-		addValue(row, 2, "", STYLE_LONG_TEXT);
-		
-		addValue(row, 3, unit.getPath(),       STYLE_LONG_TEXT);
+		addValue(row, 0, null, STYLE_LONG_TEXT);
+		addValue(row, 1, null, STYLE_TYPE);
+		addValue(row, 2, null, STYLE_LONG_TEXT);
+		addValue(row, 3, unit.getPath(),        STYLE_LONG_TEXT);
 		addValue(row, 4, unit.getType(),        STYLE_TYPE);
 		addValue(row, 5, unit.getDescription(), STYLE_LONG_TEXT);
 	}
@@ -54,7 +55,19 @@ public class XlsResultHandler implements Handler {
 		writer.close();
 	}
 	
-	private Map<Short, CellStyle> getStyles() {
+	private int getRowNum() {
+		int rowNum = this.rowNum;
+		this.rowNum++;
+		return rowNum;
+	}
+	
+	private void addValue(Row row, int cellNum, String value, short style) {
+		Cell cell = row.createCell(cellNum);
+		cell.setCellStyle(this.styles.get(style));
+		cell.setCellValue(value);
+	}
+	
+	private Map<Short, CellStyle> createStyles() {
 		Map<Short, CellStyle> map = new HashMap<Short, CellStyle>();
 		
 		Font bold = workbook.createFont();
@@ -79,55 +92,73 @@ public class XlsResultHandler implements Handler {
 		
 		return map;
 	}
-	
-	private int getRowNum() {
-		int rowNum = this.rowNum;
-		this.rowNum++;
-		return rowNum;
-	}
-	
+		
 	private CellStyle addBorder(CellStyle style, short borderStyle) {
 		style.setBorderBottom(borderStyle);
 		style.setBorderTop(borderStyle);
 		style.setBorderLeft(borderStyle);
 		style.setBorderRight(borderStyle);
-		
 		return style;
 	}
 	
 	private void createHeader() {
 		sheet.addMergedRegion(new CellRangeAddress(0,0,0,2));
 		sheet.addMergedRegion(new CellRangeAddress(0,0,3,5));
-		
-		sheet.setColumnWidth(0, 15000);
-		sheet.setColumnWidth(1, 6000);
-		sheet.setColumnWidth(2, 10000);
-		sheet.setColumnWidth(3, 15000);
-		sheet.setColumnWidth(4, 6000);
-		sheet.setColumnWidth(5, 10000);
-		
-		Row baseRow = sheet.createRow(getRowNum());
-		Row headRow = sheet.createRow(getRowNum());
-		
-		addValue(baseRow, 0, "From",        STYLE_HEADER);
-		addValue(baseRow, 1, "",            STYLE_HEADER);
-		addValue(baseRow, 2, "",            STYLE_HEADER);
-		addValue(baseRow, 3, "To",          STYLE_HEADER);
-		addValue(baseRow, 4, "",            STYLE_HEADER);
-		addValue(baseRow, 5, "",            STYLE_HEADER);
-		
-		addValue(headRow, 0, "element",     STYLE_HEADER);
-		addValue(headRow, 1, "type",        STYLE_HEADER);
-		addValue(headRow, 2, "description", STYLE_HEADER);
-		
-		addValue(headRow, 3, "element",     STYLE_HEADER);
-		addValue(headRow, 4, "type",        STYLE_HEADER);
-		addValue(headRow, 5, "description", STYLE_HEADER);
+							
+		for(List<HeaderCell> headerRow: getHeader()) {
+			Row row = sheet.createRow(getRowNum());		
+			for(HeaderCell cell : headerRow) {								
+				addValue(row, cell.getColNum(), cell.getTitle(), cell.getStyle());
+				if(cell.getWidth() != null)
+					sheet.setColumnWidth(cell.getColNum(), cell.getWidth());
+			}
+		}
 	}
 	
-	private void addValue(Row row, int cellNum, String value, short style) {
-		Cell cell = row.createCell(cellNum);
-		cell.setCellStyle(this.style.get(style));
-		cell.setCellValue(value);
+	private List<List<HeaderCell>> getHeader() {
+		List<List<HeaderCell>> header = new ArrayList<List<HeaderCell>>();
+		List<HeaderCell> baseRow = new ArrayList<HeaderCell>();
+		List<HeaderCell> headRow = new ArrayList<HeaderCell>();
+		
+		baseRow.add(new HeaderCell("from", 0, STYLE_HEADER, null));
+		baseRow.add(new HeaderCell("",     1, STYLE_HEADER, null));
+		baseRow.add(new HeaderCell("", 	   2, STYLE_HEADER, null));
+		baseRow.add(new HeaderCell("to",   3, STYLE_HEADER, null));
+		baseRow.add(new HeaderCell("",     4, STYLE_HEADER, null));
+		baseRow.add(new HeaderCell("",     5, STYLE_HEADER, null));
+		
+		headRow.add(new HeaderCell("element",     0, STYLE_HEADER, 15000));
+		headRow.add(new HeaderCell("type",        1, STYLE_HEADER, 6000));
+		headRow.add(new HeaderCell("description", 2, STYLE_HEADER, 10000));
+		headRow.add(new HeaderCell("element",     3, STYLE_HEADER, 15000));
+		headRow.add(new HeaderCell("type",        4, STYLE_HEADER, 6000));
+		headRow.add(new HeaderCell("description", 5, STYLE_HEADER, 10000));
+			
+		header.add(baseRow);
+		header.add(headRow);
+		
+		return header;
 	}
+		
+    private class HeaderCell { 	
+    	private String title;
+    	private int colNum;
+    	private short style;
+    	private Integer width;
+    	
+		public HeaderCell(String title, int colNum, short style, Integer width) {
+			this.title = title;
+			this.colNum = colNum;
+			this.style = style;
+			this.width = width;
+		}
+		
+		public String getTitle() { return title; }
+		
+		public int getColNum() { return colNum; }
+		
+		public short getStyle() { return style; }
+		
+		public Integer getWidth() { return width; }
+    }
 }
