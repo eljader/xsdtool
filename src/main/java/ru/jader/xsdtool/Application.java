0952b00a4d9b3a83d1e8c86a201e98d5919ff82e
eljader <pinkyjad@gmail.com>
2015-07-17 19:44:33 +0300

    add logger

--- src/main/java/ru/jader/xsdtool/Application.java
+++ src/main/java/ru/jader/xsdtool/Application.java
@@ -1,14 +1,42 @@
 package ru.jader.xsdtool;
 
+import java.util.Arrays;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import javax.swing.JTextArea;
+
 import ru.jader.xsdtool.gui.view.MainView;
 import ru.jader.xsdtool.gui.view.View;
 
 
 public class Application
 {
+    private static Logger log = Logger.getLogger(Application.class.getName());
+    private static JTextArea appender;
+
     public static void main( String[] args ) throws Exception
     {
-        View mainWindow = new MainView();
-        mainWindow.render();
+        try {
+            appender = new JTextArea();
+            View mainWindow = new MainView(appender);
+            mainWindow.render();
+
+            log.addHandler(new Handler() {
+
+                public void publish(LogRecord logRecord) {
+                    appender.append(logRecord.getMessage() + "\n");
+                    appender.setCaretPosition(appender.getText().length());
+                }
+
+                public void flush() {}
+                public void close() {}
+            });
+
+        } catch (Exception e) {
+            log.log(Level.SEVERE, Arrays.toString(e.getStackTrace()));
+        }
     }
 }
--- src/main/java/ru/jader/xsdtool/gui/view/MainView.java
+++ src/main/java/ru/jader/xsdtool/gui/view/MainView.java
@@ -20,83 +20,79 @@ import ru.jader.xsdtool.gui.command.LoadSchemaFileCommand;
 import ru.jader.xsdtool.gui.listener.PushButtonListener;
 import ru.jader.xsdtool.gui.listener.LoadFileListener;
 import ru.jader.xsdtool.gui.listener.SwitchButtonListener;
-import ru.jader.xsdtool.parser.Schema;
-import ru.jader.xsdtool.parser.XsdSchema;
 
 public class MainView extends FrameView {
-	
-	private static String WINDOW_NAME = "xsdtool";
-	private static int WINDOW_WIDTH = 800;
-	private static int WINDOW_HEIGHT = 600;
-	
-	private Schema schema;
-	
-	@Override
-	protected void doRender(JFrame frame) {
-		JPanel panel = getPanel(frame);
-		frame.add(panel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.setVisible(true);	
-	}
 
-	@Override
-	protected WindowAdapter getWindowListner() {
-		return null;
-	}
+    private static String WINDOW_NAME = "xsdtool";
+    private static int WINDOW_WIDTH = 800;
+    private static int WINDOW_HEIGHT = 600;
 
-	@Override
-	protected ViewSettings getViewSettings() {
-		return new ViewSettings(WINDOW_NAME, WINDOW_WIDTH, WINDOW_HEIGHT);
-	}
-	
-	protected JPanel getPanel(JFrame frame) {
-		JPanel panel = new JPanel();		
-		panel.setLayout(null);
-							
-		JComboBox<KeyValue<String, SchemaComponent>> schemaCombo = new JComboBox<KeyValue<String, SchemaComponent>>();
-		schemaCombo.setBounds(frame.getWidth() - 520, frame.getHeight() - 350, 220, 25);
-		schemaCombo.setEditable(false);
-		schemaCombo.setEditable(false);
-		panel.add(schemaCombo);
-		
-		JButton parseButton = new JButton("make template");
-		parseButton.setBounds(frame.getWidth() - 280, frame.getHeight() - 350, 120, 25);
-		parseButton.setEnabled(false);
-		panel.add(parseButton);
-		
-		schemaCombo.addActionListener(new SwitchButtonListener(parseButton));
-		parseButton.addActionListener(new PushButtonListener(new GenerateXlsxDocumentCommand(schemaCombo)));
+    @Override
+    protected void doRender(JFrame frame) {
+        JPanel panel = getPanel(frame);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JTextField filePath = new JTextField();
-		filePath.setBounds(frame.getWidth() - 520, frame.getHeight() - 400, 220, 25);
-		filePath.setEditable(false);
-		panel.add(filePath);
-				
-		JButton browseFile = new JButton("load schema");
-		browseFile.setBounds(frame.getWidth() - 280, frame.getHeight() - 400, 120, 25);
-		browseFile.addActionListener(new LoadFileListener(
-				"Load Schema",
-				new FileNameExtensionFilter(".xsd .wsdl", "xsd", "wsdl"),
-				new LoadSchemaFileCommand(filePath, schemaCombo)
-			)
-		);
-		panel.add(browseFile);
-						
-		int textAreaHeight = frame.getHeight() / 3;
-		int textAreaWidth = frame.getWidth() - 20;
-		int indentTop = frame.getHeight() - textAreaHeight - 50;
-		
-		JTextArea queryTextArea = new JTextArea(500, 200);
-		queryTextArea.setBounds(0, indentTop, textAreaWidth, textAreaHeight);
-		queryTextArea.setEditable(false);
-					
-		JScrollPane scrollPane = new JScrollPane(queryTextArea);
-		scrollPane.setBounds(0, indentTop, textAreaWidth, textAreaHeight);
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
-		panel.add(scrollPane);		
-		return panel;
-	}
+        frame.setVisible(true);
+    }
+
+    @Override
+    protected WindowAdapter getWindowListner() {
+        return null;
+    }
+
+    @Override
+    protected ViewSettings getViewSettings() {
+        return new ViewSettings(WINDOW_NAME, WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+
+    protected JPanel getPanel(JFrame frame) {
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        JComboBox<KeyValue<String, SchemaComponent>> schemaCombo = new JComboBox<KeyValue<String, SchemaComponent>>();
+        schemaCombo.setBounds(frame.getWidth() - 520, frame.getHeight() - 350, 220, 25);
+        schemaCombo.setEditable(false);
+        schemaCombo.setEditable(false);
+        panel.add(schemaCombo);
+
+        JButton parseButton = new JButton("make template");
+        parseButton.setBounds(frame.getWidth() - 280, frame.getHeight() - 350, 120, 25);
+        parseButton.setEnabled(false);
+        panel.add(parseButton);
+
+        schemaCombo.addActionListener(new SwitchButtonListener(parseButton));
+        parseButton.addActionListener(new PushButtonListener(new GenerateXlsxDocumentCommand(schemaCombo)));
+
+        JTextField filePath = new JTextField();
+        filePath.setBounds(frame.getWidth() - 520, frame.getHeight() - 400, 220, 25);
+        filePath.setEditable(false);
+        panel.add(filePath);
+
+        JButton browseFile = new JButton("load schema");
+        browseFile.setBounds(frame.getWidth() - 280, frame.getHeight() - 400, 120, 25);
+        browseFile.addActionListener(new LoadFileListener(
+                "Load Schema",
+                new FileNameExtensionFilter(".xsd .wsdl", "xsd", "wsdl"),
+                new LoadSchemaFileCommand(filePath, schemaCombo)
+            )
+        );
+        panel.add(browseFile);
+
+        int textAreaHeight = frame.getHeight() / 3;
+        int textAreaWidth = frame.getWidth() - 20;
+        int indentTop = frame.getHeight() - textAreaHeight - 50;
+
+        JTextArea queryTextArea = new JTextArea(500, 200);
+        queryTextArea.setBounds(0, indentTop, textAreaWidth, textAreaHeight);
+        queryTextArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(queryTextArea);
+        scrollPane.setBounds(0, indentTop, textAreaWidth, textAreaHeight);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        panel.add(scrollPane);
+        return panel;
+    }
 }
