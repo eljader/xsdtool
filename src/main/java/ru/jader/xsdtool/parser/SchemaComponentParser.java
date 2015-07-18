31b2297045fc4724e4436dee5f71bbf39ff7107b
jader <pinkyjad@gmail.com>
2015-07-18 21:08:07 +0300

    add support simple type parse

--- src/main/java/ru/jader/xsdtool/parser/SchemaComponentParser.java
+++ src/main/java/ru/jader/xsdtool/parser/SchemaComponentParser.java
@@ -8,59 +8,62 @@ import org.apache.xmlbeans.SchemaProperty;
 import org.apache.xmlbeans.SchemaType;
 
 public abstract class SchemaComponentParser {
-	
-	public void parse(SchemaComponent component) throws ParseComponentException {
-		SchemaType type = null;
-		
-		if(component instanceof SchemaType)
-			type = (SchemaType) component;
-			
-		if(component instanceof SchemaGlobalElement)
-			type = ((SchemaGlobalElement) component).getType();
-			
-		if(type == null) 
-			throw new RuntimeException(String.format("not implemented for %s", component.getClass()));
-		
-		parseType(type, new String());
-	}
 
-	private void parseElement(SchemaLocalElement element, String path) throws ParseComponentException {
-		SchemaType type = element.getType();
-		boolean isNotRecursive = !this.isRecursive(path, element);
-		path = this.rebuildPath(path, element);
+    public void parse(SchemaComponent component) throws ParseComponentException {
+        SchemaType type = null;
 
-		if(type.getContentModel() != null && isNotRecursive)
-			parseType(type, path); 
-		else
-			this.process(path, element);
-	}
+        if(component instanceof SchemaType)
+            type = (SchemaType) component;
 
-	private void parseType(SchemaType type, String path) throws ParseComponentException {
-		SchemaProperty[] attributes = type.getAttributeProperties();
+        if(component instanceof SchemaGlobalElement)
+            type = ((SchemaGlobalElement) component).getType();
 
-		for (int i = 0; i < attributes.length; i++) {
-			SchemaProperty attribute = attributes[i];
-			this.process(path, attribute);
-		}
+        if(type == null)
+            throw new RuntimeException(String.format("not implemented for %s", component.getClass()));
 
-		SchemaParticle particle = type.getContentModel();
-		
-		if(particle.countOfParticleChild() == 0) {
-			parseElement((SchemaLocalElement) particle, path);
-		} 
-		else {
-			SchemaParticle[] childes = particle.getParticleChildren();
+        parseType(type, new String());
+    }
 
-			for (int i = 0; i < childes.length; i++) {
-				SchemaLocalElement element = (SchemaLocalElement) childes[i];
-				parseElement(element, path);
-			}
-		}
-	}
-		
-	protected abstract String rebuildPath(String path, SchemaLocalElement element);
-	
-	protected abstract void process(String path, Object parseObject) throws ParseComponentException;
-	
-	protected abstract boolean isRecursive(String path, SchemaLocalElement element);
+    private void parseElement(SchemaLocalElement element, String path) throws ParseComponentException {
+        SchemaType type = element.getType();
+        boolean isNotRecursive = !this.isRecursive(path, element);
+        path = this.rebuildPath(path, element);
+
+        if(type.getContentModel() != null && isNotRecursive)
+            parseType(type, path);
+        else
+            this.process(path, element);
+    }
+
+    private void parseType(SchemaType type, String path) throws ParseComponentException {
+        SchemaProperty[] attributes = type.getAttributeProperties();
+
+        for (int i = 0; i < attributes.length; i++) {
+            SchemaProperty attribute = attributes[i];
+            this.process(path, attribute);
+        }
+
+        SchemaParticle particle = type.getContentModel();
+
+        if(particle == null) {
+            this.process(path, type);
+        }
+        else if(particle.countOfParticleChild() == 0) {
+            parseElement((SchemaLocalElement) particle, path);
+        }
+        else {
+            SchemaParticle[] childes = particle.getParticleChildren();
+
+            for (int i = 0; i < childes.length; i++) {
+                SchemaLocalElement element = (SchemaLocalElement) childes[i];
+                parseElement(element, path);
+            }
+        }
+    }
+
+    protected abstract String rebuildPath(String path, SchemaLocalElement element);
+
+    protected abstract void process(String path, Object parseObject) throws ParseComponentException;
+
+    protected abstract boolean isRecursive(String path, SchemaLocalElement element);
 }
--- src/main/java/ru/jader/xsdtool/parser/SimpleSchemaComponentParser.java
+++ src/main/java/ru/jader/xsdtool/parser/SimpleSchemaComponentParser.java
@@ -20,153 +20,157 @@ import ru.jader.xsdtool.parser.handler.ParseHandlerException;
 import ru.jader.xsdtool.parser.model.XsdUnit;
 
 public class SimpleSchemaComponentParser extends SchemaComponentParser {
-	
-	private static String ELEMENT_DELIMITER = "/";
-	private static String ATTR_DELIMITER = ELEMENT_DELIMITER + "@";
-	
-	private ParseHandler handler;
-	
-	public SimpleSchemaComponentParser(ParseHandler handler) throws XmlException, IOException {
-		this.handler = handler;
-	}
-	
-	@Override
-	protected String rebuildPath(String path, SchemaLocalElement element) {
-		return rebuildPath(path, element.getName().getLocalPart(), ELEMENT_DELIMITER);
-	}
-	
-	private String rebuildPath(String path, String name, String delimiter) {
-		return path + delimiter + name;  	
-	}
-	
-	@Override
-	protected void process(String path, Object parseObject) throws ParseComponentException {
-		try {
-			handler
-				.handle(createXsdUnit(path, parseObject))
-			;
-		} catch (ParseHandlerException e) { 
-			throw new ParseComponentException(e); 
-		}
-	}
-	
-	@Override
-	protected boolean isRecursive(String path, SchemaLocalElement element) {
-		//TODO make namespace sensitive
-		return (path.indexOf(String.format("/%s/", element.getName().getLocalPart())) == -1) ? false : true;	
-	}
-	
-	private XsdUnit createXsdUnit(String path, Object parseObject) {	
-		SchemaType type = null;
-		StringBuilder description = new StringBuilder();
-				
-		if(parseObject instanceof SchemaLocalElement) {		
-			SchemaLocalElement element = (SchemaLocalElement) parseObject;		
-			type = element.getType(); 
-			assembleDescription(element, description);		
-		}
-		else if(parseObject instanceof SchemaProperty) {		
-			SchemaProperty attribute = (SchemaProperty) parseObject;
-			path = rebuildPath(path, attribute.getName().getLocalPart(), ATTR_DELIMITER);	
-			type = attribute.getType();
-			assembleDescription(type, description);
-		}
-		else {
-			throw new RuntimeException(String.format("not implemented for %s", parseObject.getClass()));
-		}
 
-		return new XsdUnit(path, assembleType(type), description.toString());
-	}
+    private static String ELEMENT_DELIMITER = "/";
+    private static String ATTR_DELIMITER = ELEMENT_DELIMITER + "@";
 
-	private String assembleType(SchemaType type) {		
-		if(type.isPrimitiveType())
-			return type.getName().getLocalPart();
-		else if(type.getContentModel() != null)
-			return type.getName().toString();
-		else
-			return type.getBaseType().getName().getLocalPart();
-	}
+    private ParseHandler handler;
 
-	private void assembleDescription(SchemaLocalElement element, StringBuilder description) {
-		SchemaAnnotation annotation = element.getAnnotation();
+    public SimpleSchemaComponentParser(ParseHandler handler) throws XmlException, IOException {
+        this.handler = handler;
+    }
 
-		if(annotation != null)
-			assembleDescription(annotation, description)
-		;
+    @Override
+    protected String rebuildPath(String path, SchemaLocalElement element) {
+        return rebuildPath(path, element.getName().getLocalPart(), ELEMENT_DELIMITER);
+    }
 
-		assembleDescription(element.getType(), description);
-	}
+    private String rebuildPath(String path, String name, String delimiter) {
+        return path + delimiter + name;
+    }
 
-	private void assembleDescription(SchemaType type, StringBuilder description) {
-		String[] enums = getEnum(type);
-		SchemaAnnotation annotation = type.getAnnotation();
+    @Override
+    protected void process(String path, Object parseObject) throws ParseComponentException {
+        try {
+            handler
+                .handle(createXsdUnit(path, parseObject))
+            ;
+        } catch (ParseHandlerException e) {
+            throw new ParseComponentException(e);
+        }
+    }
 
-		if(annotation != null)
-			assembleDescription(annotation, description)
-		;
+    @Override
+    protected boolean isRecursive(String path, SchemaLocalElement element) {
+        //TODO make namespace sensitive
+        return (path.indexOf(String.format("/%s/", element.getName().getLocalPart())) == -1) ? false : true;
+    }
 
-		if(!type.isPrimitiveType()) {
-			for (Entry<String, Integer> facetEntry : getFacetMap().entrySet()) {
-				XmlAnySimpleType facet = type.getFacet(facetEntry.getValue());
+    private XsdUnit createXsdUnit(String path, Object parseObject) {
+        SchemaType type = null;
+        StringBuilder description = new StringBuilder();
 
-				if(facet != null)
-					addLineSeparator(description)
-						.append(facetEntry.getKey() + "=" + facet.getStringValue())
-				;
-			}
-		}
+        if(parseObject instanceof SchemaLocalElement) {
+            SchemaLocalElement element = (SchemaLocalElement) parseObject;
+            type = element.getType();
+            assembleDescription(element, description);
+        }
+        else if(parseObject instanceof SchemaProperty) {
+            SchemaProperty attribute = (SchemaProperty) parseObject;
+            path = rebuildPath(path, attribute.getName().getLocalPart(), ATTR_DELIMITER);
+            type = attribute.getType();
+            assembleDescription(type, description);
+        }
+        else if(parseObject instanceof SchemaType) {
+            type = (SchemaType) parseObject;
+            assembleDescription(type, description);
+        }
+        else {
+            throw new RuntimeException(String.format("not implemented for %s", parseObject.getClass()));
+        }
 
-		if(enums != null)
-			addLineSeparator(description).append(Arrays.toString(enums))
-		;
-	}
+        return new XsdUnit(path, assembleType(type), description.toString());
+    }
 
-	private void assembleDescription(SchemaAnnotation annotation, StringBuilder description) {
-		XmlObject[] infos = annotation.getUserInformation();
-		for (int i = 0; i < infos.length; i++) {
-			XmlObject info = infos[i];
-			addLineSeparator(description).append(info.newCursor().getTextValue().trim());
-		}
-	}
+    private String assembleType(SchemaType type) {
+        if(type.isPrimitiveType())
+            return type.getName().getLocalPart();
+        else if(type.getContentModel() != null)
+            return type.getName().toString();
+        else
+            return type.getBaseType().getName().getLocalPart();
+    }
 
-	private String[] getEnum(SchemaType type) {
-		SchemaType enumType = type.getBaseEnumType();
+    private void assembleDescription(SchemaLocalElement element, StringBuilder description) {
+        SchemaAnnotation annotation = element.getAnnotation();
 
-		if(enumType != null) {
-			SchemaStringEnumEntry[] enumEntries = enumType.getStringEnumEntries();
-			String[] enums = new String[enumEntries.length];
+        if(annotation != null)
+            assembleDescription(annotation, description)
+        ;
 
-			for(int i = 0; i < enumEntries.length; i++)
-				enums[i] = enumEntries[i].getEnumName()
-			;
+        assembleDescription(element.getType(), description);
+    }
 
-			return enums;
-		}
+    private void assembleDescription(SchemaType type, StringBuilder description) {
+        String[] enums = getEnum(type);
+        SchemaAnnotation annotation = type.getAnnotation();
 
-		return null;
-	}
+        if(annotation != null)
+            assembleDescription(annotation, description)
+        ;
 
-	private Map<String, Integer> getFacetMap() {
-		Map<String, Integer>  map = new HashMap<String, Integer> ();
+        if(!type.isPrimitiveType()) {
+            for (Entry<String, Integer> facetEntry : getFacetMap().entrySet()) {
+                XmlAnySimpleType facet = type.getFacet(facetEntry.getValue());
 
-		map.put("LENGTH"          ,SchemaType.FACET_LENGTH);
-		map.put("MIN_LENGTH"      ,SchemaType.FACET_MIN_LENGTH);
-		map.put("MAX_LENGTH"      ,SchemaType.FACET_MAX_LENGTH);
-		map.put("MIN_EXCLUSIVE"   ,SchemaType.FACET_MIN_EXCLUSIVE);
-		map.put("MIN_INCLUSIVE"   ,SchemaType.FACET_MIN_INCLUSIVE);
-		map.put("MAX_INCLUSIVE"   ,SchemaType.FACET_MAX_INCLUSIVE);
-		map.put("MAX_EXCLUSIVE"   ,SchemaType.FACET_MAX_EXCLUSIVE);
-		map.put("TOTAL_DIGITS"    ,SchemaType.FACET_TOTAL_DIGITS);
-		map.put("FRACTION_DIGITS" ,SchemaType.FACET_FRACTION_DIGITS);
-		map.put("PATTERN"         ,SchemaType.FACET_PATTERN);
+                if(facet != null)
+                    addLineSeparator(description)
+                        .append(facetEntry.getKey() + "=" + facet.getStringValue())
+                ;
+            }
+        }
 
-		return map;
-	}
+        if(enums != null)
+            addLineSeparator(description).append(Arrays.toString(enums))
+        ;
+    }
 
-	private StringBuilder addLineSeparator(StringBuilder builder) {
-		if(builder.length() > 0)
-			builder.append(System.getProperty("line.separator"));
-		
-		return builder;
-	}
+    private void assembleDescription(SchemaAnnotation annotation, StringBuilder description) {
+        XmlObject[] infos = annotation.getUserInformation();
+        for (int i = 0; i < infos.length; i++) {
+            XmlObject info = infos[i];
+            addLineSeparator(description).append(info.newCursor().getTextValue().trim());
+        }
+    }
+
+    private String[] getEnum(SchemaType type) {
+        SchemaType enumType = type.getBaseEnumType();
+
+        if(enumType != null) {
+            SchemaStringEnumEntry[] enumEntries = enumType.getStringEnumEntries();
+            String[] enums = new String[enumEntries.length];
+
+            for(int i = 0; i < enumEntries.length; i++)
+                enums[i] = enumEntries[i].getEnumName()
+            ;
+
+            return enums;
+        }
+
+        return null;
+    }
+
+    private Map<String, Integer> getFacetMap() {
+        Map<String, Integer>  map = new HashMap<String, Integer> ();
+
+        map.put("LENGTH"          ,SchemaType.FACET_LENGTH);
+        map.put("MIN_LENGTH"      ,SchemaType.FACET_MIN_LENGTH);
+        map.put("MAX_LENGTH"      ,SchemaType.FACET_MAX_LENGTH);
+        map.put("MIN_EXCLUSIVE"   ,SchemaType.FACET_MIN_EXCLUSIVE);
+        map.put("MIN_INCLUSIVE"   ,SchemaType.FACET_MIN_INCLUSIVE);
+        map.put("MAX_INCLUSIVE"   ,SchemaType.FACET_MAX_INCLUSIVE);
+        map.put("MAX_EXCLUSIVE"   ,SchemaType.FACET_MAX_EXCLUSIVE);
+        map.put("TOTAL_DIGITS"    ,SchemaType.FACET_TOTAL_DIGITS);
+        map.put("FRACTION_DIGITS" ,SchemaType.FACET_FRACTION_DIGITS);
+        map.put("PATTERN"         ,SchemaType.FACET_PATTERN);
+
+        return map;
+    }
+
+    private StringBuilder addLineSeparator(StringBuilder builder) {
+        if(builder.length() > 0)
+            builder.append(System.getProperty("line.separator"));
+
+        return builder;
+    }
 }
